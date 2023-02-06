223ce58debaf3c50c87b4fa922f7bbca26b0fc1a
mehul <you@example.com>
2023-02-06 18:17:40 +0530

    build

---
+++ build/precache-manifest.1d3816a87fe3155926b74b701193f68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0722297bc070d347d000",
+    "url": "/static/css/main.839e16f0.chunk.css"
+  },
+  {
+    "revision": "0722297bc070d347d000",
+    "url": "/static/js/main.0025c00a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06e6d613194e76a71a52",
+    "url": "/static/js/2.46cde506.chunk.js"
+  },
+  {
+    "revision": "8c6dfc29d70a9c1f67dc0eeec3d32cac",
+    "url": "/static/media/brandBanner.8c6dfc29.jpg"
+  },
+  {
+    "revision": "ea21719b70ba2c2b9cb5652a766715aa",
+    "url": "/index.html"
+  }
+];
